b3802c0ac131f72d69d2991cc955e3f3d6a15280
xuefeiyuluo <164814491@qq.com>
2018-03-16 18:20:27 +0800

    2018-03-16

---
+++ BorrowMoney/Classes/UserCenter/Model/OrderDetailModel.swift
@@ -0,0 +1,13 @@
+//
+//  OrderDetailModel.swift
+//  BorrowMoney
+//
+//  Created by 雪飞雨落 on 2018/3/14.
+//  Copyright © 2018年 sparrow. All rights reserved.
+//
+
+import UIKit
+
+class OrderDetailModel: NSObject {
+
+}
---
+++ BorrowMoney/Classes/UserCenter/View/OrderDetailInfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderDetailInfoCell.swift
+//  BorrowMoney
+//
+//  Created by 雪飞雨落 on 2018/3/8.
+//  Copyright © 2018年 sparrow. All rights reserved.
+//
+
+import UIKit
+
+class OrderDetailInfoCell: BasicViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
